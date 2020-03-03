f358441e4ed3b878d21c7e829e7392e45d4c1c37
ktfjulien <ktfjulien@gmail.com>
2020-03-03 04:04:26 -0800

    Add files via upload
    MangaViewer portion, named Josh (for some reason...)

---
+++ Josh/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Josh {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.FileBox = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TopMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SelectMaster = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.browser = new System.Windows.Forms.FolderBrowserDialog();
+            this.MainList = new System.Windows.Forms.ListView();
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.MainPic = new System.Windows.Forms.PictureBox();
+            this.TopMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(784, 758);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(369, 758);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FileBox
+            // 
+            this.FileBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FileBox.FormattingEnabled = true;
+            this.FileBox.Location = new System.Drawing.Point(412, 760);
+            this.FileBox.Name = "FileBox";
+            this.FileBox.Size = new System.Drawing.Size(366, 21);
+            this.FileBox.TabIndex = 2;
+            this.FileBox.SelectedIndexChanged += new System.EventHandler(this.FileBox_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // TopMenu
+            // 
+            this.TopMenu.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.TopMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.TopMenu.Location = new System.Drawing.Point(0, 0);
+            this.TopMenu.Name = "TopMenu";
+            this.TopMenu.Size = new System.Drawing.Size(998, 24);
+            this.TopMenu.TabIndex = 5;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SelectMaster});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // SelectMaster
+            // 
+            this.SelectMaster.Image = ((System.Drawing.Image)(resources.GetObject("SelectMaster.Image")));
+            this.SelectMaster.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SelectMaster.Name = "SelectMaster";
+            this.SelectMaster.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.SelectMaster.Size = new System.Drawing.Size(223, 22);
+            this.SelectMaster.Text = "&Select Master Folder";
+            this.SelectMaster.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.customizeToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "&Tools";
+            // 
+            // customizeToolStripMenuItem
+            // 
+            this.customizeToolStripMenuItem.Name = "customizeToolStripMenuItem";
+            this.customizeToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.customizeToolStripMenuItem.Text = "&Customize";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // browser
+            // 
+            this.browser.Description = "Select Master Folder";
+            this.browser.ShowNewFolderButton = false;
+            // 
+            // MainList
+            // 
+            this.MainList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MainList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainList.HideSelection = false;
+            this.MainList.Location = new System.Drawing.Point(0, 24);
+            this.MainList.Name = "MainList";
+            this.MainList.Size = new System.Drawing.Size(182, 765);
+            this.MainList.TabIndex = 6;
+            this.MainList.UseCompatibleStateImageBehavior = false;
+            this.MainList.View = System.Windows.Forms.View.List;
+            this.MainList.SelectedIndexChanged += new System.EventHandler(this.MainList_SelectedIndexChanged);
+            this.MainList.DoubleClick += new System.EventHandler(this.MainList_DoubleClick);
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // MainPic
+            // 
+            this.MainPic.Location = new System.Drawing.Point(200, 27);
+            this.MainPic.Name = "MainPic";
+            this.MainPic.Size = new System.Drawing.Size(791, 726);
+            this.MainPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MainPic.TabIndex = 8;
+            this.MainPic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(998, 789);
+            this.Controls.Add(this.MainPic);
+            this.Controls.Add(this.MainList);
+            this.Controls.Add(this.TopMenu);
+            this.Controls.Add(this.FileBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Manga Viewer";
+            this.TopMenu.ResumeLayout(false);
+            this.TopMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox FileBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip TopMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SelectMaster;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FolderBrowserDialog browser;
+        private System.Windows.Forms.ListView MainList;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.PictureBox MainPic;
+    }
+}
+
---
+++ Josh/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Josh {
+    public partial class Form1 : Form {
+        string master = "";
+        public Form1() {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+            if (keyData == (Keys.Right)) {
+                button1.PerformClick();
+                return true;
+            }
+
+            if (keyData == (Keys.Left)) {
+                button2.PerformClick();
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //imageList1.Images.Add(Image.FromFile(@"C:\LOGO.png"));
+            try {
+                FileBox.SelectedIndex = FileBox.SelectedIndex + 1;
+            } catch {
+
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+            DialogResult result = browser.ShowDialog();
+            if (result == DialogResult.OK) {
+                master = browser.SelectedPath;
+                string[] m = Directory.GetDirectories(master);
+
+                foreach (var v in m) {
+                    MainList.Items.Add(v.TrimEnd(Path.DirectorySeparatorChar).Split(Path.DirectorySeparatorChar).Last());
+                    // MainList.Items.Add(v);
+                }
+            }
+        }
+
+        private void MainList_DoubleClick(object sender, EventArgs e) {
+            /*FileBox.Items.Clear();
+            imageList1.Images.Clear();
+            string[] d = Directory.GetFiles(master + @"\" +  MainList.SelectedItems[0].Text);
+
+            foreach (var v in d) {
+                FileBox.Items.Add(v);
+                imageList1.Images.Add(Image.FromFile(v));
+            }
+
+            FileBox.SelectedIndex = 0;*/
+
+            //comboBox1.Items.Add("worked");
+        }
+
+        private void FileBox_SelectedIndexChanged(object sender, EventArgs e) {
+            MainPic.Image = Image.FromFile(FileBox.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (FileBox.SelectedIndex != 0) {
+                FileBox.SelectedIndex = FileBox.SelectedIndex - 1;
+            }
+        }
+
+        private void MainList_SelectedIndexChanged(object sender, EventArgs e) {
+            FileBox.Items.Clear();
+            imageList1.Images.Clear();
+            string[] d = Directory.GetFiles(master + @"\" +  MainList.SelectedItems[0].Text);
+
+            foreach (var v in d) {
+                FileBox.Items.Add(v);
+                imageList1.Images.Add(Image.FromFile(v));
+            }
+
+            FileBox.SelectedIndex = 0;
+        }
+    }
+}
